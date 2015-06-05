b8d9f92257aac1c76155232c45db3d92435c060b
grzegorz <grzegorz.kuzlo@gmail.com>
2015-06-05 12:50:34 +0200

    v

---
+++ src/grid/UI.GridButton.js
@@ -0,0 +1,39 @@
+/**
+ * @class UI.GridButton
+ */
+UI.GridButton = Class.create({
+	/**
+	 * 
+	 * @param config
+	 */
+    initialize: function(config) {
+        this.config = Object.extend({
+            inside: window.document.body
+        }, config || {});
+
+        this.render();
+    },
+    /**
+     * @method render
+     */ 
+    render: function() {
+    	var h = this;
+    	
+    	h.buttonDiv = new Element("DIV", {
+    		style: "border-radius:3px; width:26px; height:26px; line-height:26px; background-color:#525070; margin:2px; display:flex; justify-content:center; align-items:center",
+    		class: "hvr-radial-out"
+    	});
+    	h.config.inside.insert(h.buttonDiv);
+    	
+    	h.img = new Element("IMG", {
+    		src: $ICON(h.config.icon)
+    	});
+    	h.buttonDiv.insert(h.img);
+    	
+    	h.buttonDiv.on("click", function(e) {
+    		if (h.config.onClick) {
+    			h.config.onClick();
+    		}
+    	});
+    }
+});
---
+++ src/grid/UI.GridToolbar.js
@@ -0,0 +1,45 @@
+/**
+ * @class UI.GridToolbar
+ */
+UI.GridToolbar = Class.create({
+	/**
+	 * 
+	 * @param config
+	 */
+    initialize: function(config) {
+        this.config = Object.extend({
+            inside: window.document.body,
+            buttons: []
+        }, config || {});
+
+        this.render();
+    },
+    /**
+     * @method render 
+     */ 
+    render: function() {
+    	var h = this;
+
+    	h.material = new Element("DIV", {
+    		style: "position:absolute; display:flex; flex-direction:row; top:0px; left:0px; right:0px; bottom:0px"
+    	});
+    	h.config.inside.insert(h.material);
+
+    	h.setButtons(h.config.buttons);
+    },
+    /**
+     * 
+     * @param buttons
+     */
+    setButtons: function(buttons) {
+    	var h = this;
+
+    	var i = 0;
+    	for (i=0; i<buttons.length; i++) {
+    		var buttonConfig = buttons[i];
+    			buttonConfig.inside = h.material; 
+
+    		var button = new UI.GridButton(buttonConfig);
+    	}
+    }
+});
